3838391b39b99f9f69e22c7cf260632deee059db
Prometheus92 <abeni.pro1@gmail.com>
2023-03-20 12:38:29 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a sturct with elements name, age and owner.
+ * @name: name of the dog; type char *
+ * @age: age of the dog; type float
+ * @owner: name of the owner; type char*
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
