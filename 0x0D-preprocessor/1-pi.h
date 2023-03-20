e4f8b78a6e6e57b878441b8a0c77251c713c8a45
Prometheus92 <abeni.pro1@gmail.com>
2023-03-20 14:32:27 -0400

    mandatory tasks

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,4 @@
+#ifndef PI_NOT_SEEN
+#define PI_NOT_SEEN
+#define PI 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Prints the name of the file it compiled form, followed by
+ * a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
