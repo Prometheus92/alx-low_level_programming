3ea2433c4c70f6334def0350efd884e02a805482
Prometheus92 <118185840+Prometheus92@users.noreply.github.com>
2023-03-22 17:33:59 -0400

    msg

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
-#include "main.h"
+#include <stdlib.h>
+#include "function_pointers.h"
+
 /**
- * print_name - Prints a name
- * @name: name
- * @f: pointer to a function
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-void print_name(char *name, int void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
 		return;
