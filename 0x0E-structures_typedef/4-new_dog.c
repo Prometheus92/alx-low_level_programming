f8237af1e8aa8856b111c7661b56a9eda6c6d197
Prometheus92 <abeni.pro1@gmail.com>
2023-08-15 05:48:55 +0300

    update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _copy - makes a copy of the string
+ * @str: String to be copied
+ *
+ * Return: copy string
+ */
+char *_copy(char *str)
+{
+	char *cp;
+	int i, j;
+
+	if (src == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	cp = malloc(size(char) * (i + 1));
+	if (cp == NULL)
+		return (NULL);
+	for (j = 0; str[j] != '\0'; j++)
+		cp[j] = str[j];
+	cp[j] = '\0';
+	return (cp);
+}
+
+/**
  * new_dog - creates a new dog using the typedef reference
  * @name: name of the dog
  * @age: age of the dog
@@ -16,8 +40,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	dog->name = _copy(name);
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = _copy(owner);
 	return (dog);
 }
