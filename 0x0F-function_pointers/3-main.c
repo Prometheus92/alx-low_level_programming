2ae549151faa2f6793291b95ff63dbb1007dd646
Prometheus92 <abeni.pro1@gmail.com>
2023-08-17 05:58:30 +0300

    added null saftey

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,6 +18,12 @@ int main(int argc, char *argv[])
 	}
 
 	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	printf("%d\n", (*func)(a, b));
