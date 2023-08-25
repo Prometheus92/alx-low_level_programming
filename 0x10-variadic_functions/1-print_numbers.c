21e0631b19d2f8e0ab9c7e7152d1403284fc958b
Prometheus92 <abeni.pro1@gmail.com>
2023-08-25 12:17:59 +0300

    added null saftey

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d%s", va_arg(ptr, int), separator);
+		if (separator == NULL)
+			printf("%d", va_arg(ptr, int));
+		else
+			printf("%d%s", va_arg(ptr, int), separator);
 	}
 	printf("%d\n", va_arg(ptr, int));
 	va_end(ptr);
