ebd4d01c20c2e046f81b32266fd5af1d3f1f3cf4
Prometheus92 <abeni.pro1@gmail.com>
2023-08-25 12:40:29 +0300

    added null saftey

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,18 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
+ * r_str - returns string
+ * @str: sting passed to it
+ *
+ * Return: returns string
+ */
+char *r_str(char *str)
+{
+	if (str == NULL)
+		return ("(nil)");
+	return (str);
+}
+/**
  * print_strings - prints strings, followed by a new line
  * @separator: string to be printed between the strings
  * @n: the number of strings passed to the function
@@ -16,9 +28,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (separator == NULL || i == n - 1)
-			printf("%s", va_arg(ptr, char *));
+			printf("%s", r_str(va_arg(ptr, char *)));
 		else
-			printf("%s%s", va_arg(ptr, char *), separator);
+			printf("%s%s", r_str(va_arg(ptr, char *)), separator);
 	}
 	printf("\n");
 	va_end(ptr);
